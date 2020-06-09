3b50fe8eccbf4030094fa162be41c39c08416fab
한성재 <fhqhxm8899@gmail.com>
2020-06-09 20:46:51 +0900

    start

--- GimalGwajae1/GimalGwajae1.cpp
+++ GimalGwajae1/GimalGwajae1.cpp
@@ -1,20 +1,41 @@
-﻿// GimalGwajae1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include<bangtal.h>
+#include <stdio.h> 
 
-#include <iostream>
+SceneID scene0, scene1;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}dasas
+ObjectID startButton, endButton, playButton;
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void startGame() {
+	hideObject(startButton);
+	hideObject(endButton);
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	enterScene(scene1);
+}
+
+void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+	if (object == endButton) {
+		endGame();
+	}
+	else if (object == startButton) {
+		startGame();
+	}
+}
+
+int main() {
+
+	setMouseCallback(mouseCallback);
+
+	scene0 = createScene("대기화면", "images/배경1.jpg");
+	scene1 = createScene("게임시작", "images/배경1.jpg");
+
+
+	startButton = createObject("images/start.png");
+	locateObject(startButton, scene0, 590, 300);
+	showObject(startButton);
+
+	endButton = createObject("images/end.png");
+	locateObject(endButton, scene0, 590, 250);
+	showObject(endButton);
+
+	startGame(scene0);
+}
